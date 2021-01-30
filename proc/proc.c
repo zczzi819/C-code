3fd727d403f07b9abe6d296a1df14bff05a8a655
zczzi819 <1220630049@qq.com>
2021-01-30 21:43:34 +0800

    进度条代码

---
+++ proc/proc.c
@@ -0,0 +1,21 @@
+#include"proc.h"
+
+void proc()
+{
+  char bar[NUM];
+  memset(bar,'\0',sizeof(bar));
+  const char *lable="|/-\\";
+  int i=0;
+  while(i<100)
+  {
+    
+    
+    fflush(stdout);
+   bar[i++]='#'; 
+    usleep(30000);
+    printf("[%-100s][%3d%%][%c]\r",bar,i,lable[i%4]);
+
+  }
+  printf("\n");
+  
+}
---
+++ proc/proc.h
@@ -0,0 +1,7 @@
+#pragma once 
+#include<string.h>
+#include<unistd.h>
+
+#include<stdio.h>
+#define NUM 101
+extern  void proc();
